cd8ae49ad5274fc3ed9b81a00b8bb5fc08589848
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:28:25 -0300

    Update ConexaoBD.java

--- src/ConexaoBD.java
+++ src/ConexaoBD.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ConexaoBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+    private static ConexaoBD instance;
+    private Connection connection;
+    private String url = "jdbc:mysql://localhost:3306/sua_database";
+    private String username = "usuario";
+    private String password = "senha";
+
+    private ConexaoBD() throws SQLException {
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException("Erro ao conectar ao banco de dados.");
+        }
+    }
+
+    public static ConexaoBD getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new ConexaoBD();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new ConexaoBD();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }

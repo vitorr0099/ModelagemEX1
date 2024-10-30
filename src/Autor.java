77e3e7b00a74385a1351cae4d0715222a8ff5761
Vitor Hugo Ramos <127155752+vitorr0099@users.noreply.github.com>
2024-10-30 19:27:02 -0300

    Update Autor.java

--- src/Autor.java
+++ src/Autor.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Autor {
+// Classe Autor
+public class Autor {
+    private int idAutor;
+    private String nome;
+    private String nacionalidade;
+
+    // Getters e Setters
+    public int getIdAutor() {
+        return idAutor;
+    }
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
+
+// Classe Livro
+public class Livro {
+    private int idLivro;
+    private String titulo;
+    private int anoPublicacao;
+    private int idAutor; // Chave estrangeira para Autor
+
+    // Getters e Setters
+    public int getIdLivro() {
+        return idLivro;
+    }
+    public void setIdLivro(int idLivro) {
+        this.idLivro = idLivro;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+    public int getIdAutor() {
+        return idAutor;
+    }
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
 }
